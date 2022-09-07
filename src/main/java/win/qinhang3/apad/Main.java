3211850f37bc1557d2b687a0ede440a7087a5306
hang.qin <qinhang3@hotmail.com>
2022-09-07 17:00:34 +0800

    actions

--- src/main/java/win/qinhang3/apad/Main.java
+++ src/main/java/win/qinhang3/apad/Main.java
@@ -24,23 +24,25 @@ import java.util.function.Consumer;
  * @create: 2022/2/11
  **/
 public class Main {
-    private static final ImageUtil imageUtil = new ImageUtil(1);
+    private static final ImageUtil imageUtil = new ImageUtil(10);
 
     public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
         Suit suit = new NormalSuit();
         suit.getMap().target(Calendar.getInstance());
-        Collections.shuffle(suit.getPieces());
+//        Collections.shuffle(suit.getPieces());
         suit.run((map,finish) -> {
             imageUtil.add(map);
             BufferedImage draw = imageUtil.draw();
             try {
                 ImageIO.write(draw, "png", new File("test.png"));
-                finish.set(true);
+                finish.set(false);
             } catch (Exception e){
                 e.printStackTrace();
             }
 //            System.exit(0);
         });
+        System.out.println(System.currentTimeMillis() - start);
     }
 
 
